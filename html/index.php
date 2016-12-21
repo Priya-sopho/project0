a078db2677267fb2943e1c84110d48eede1d3d4b
Priya <prrani28@gmail.com>
2016-12-21 15:29:56 +0530

    Created new file views which contain footer and header .php files

--- html/index.php
+++ html/index.php
@@ -1,9 +1,10 @@
-<!Doctype html>
-<html>
-<head>
- <title>hello</title>
-</head>
-<body>
- <h1>Hello! Folks</h1>
-</body>
-</html> 
+<!--
+ index.php
+ This is home page for the website-->
+
+<?php 
+    require_once('../includes/helpers.php');
+    render('header',array('title'=>'Three Ace'));
+    echo("Hello");
+    render("footer");
+ ?>   
---
+++ views/header.php
@@ -0,0 +1,7 @@
+<!Doctype html>
+<html>
+ <head>
+ <title><?php echo htmlspecialchars($title)?></title>
+ </head>
+ <body>
+  <h1><?php echo htmlspecialchars($title) ?></h1>
